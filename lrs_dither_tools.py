ee7a8baffc3b63ce56ef0c77c70494b42e79d1c0
Sarah Kendrew <sarahaskendrew@gmail.com>
2019-07-29 16:46:03 -0400

    minor changes; made test pattern smaller

--- lrs_dither_tools.py
+++ lrs_dither_tools.py
@@ -130,9 +130,154 @@ class LRSPattern(object):
 		#self.run_checks()
 		
 		
+	
+			
+
+	def to_absolute(self):
+		
+		'''
+		This function will change the coordinates from relative to absolute. This is ONLY applicable for patterns defined in the 'det-rel' frame; it will take the pattern, identify the reference coordinates for the given mode, and calculate the absolute detector pixels.
+		
+		Parameters:
+		-----------
+		none
+		
+		Returns:
+		--------
+		- the input pattern with updated positions, and the reference frame set to 'det-abs' in the metadata. if the out keyword was set, the original pattern will be the same and a new pattern is created
+		
+		
+		
+		'''
+		
+		# first check that the input pattern is defined in relative coordinates
+		assert ('rel' in self.frame[0]), "The input pattern is not in relative coordinates!"
+		
+		print('Converting pattern to absolute coordinates.....')
+		# if the input coordinates are relative, load the coordinates so we can do the translation
+		coords = lrs_gencoords(mode=self.mode, frame=self.frame[0][:3], verbose=False)
+		
+		# convert the reference attribute to a list
+		ref_tmp = (self.ref[0]).split(',')
+		# strip out whitespaces in case there are any:
+		refs = [r.strip() for r in ref_tmp]
+		
+		# the (number of columns - 1) needs to be == (2 * the number of entries in the reference list)
+		n_coordcols = len(self.patt.colnames) - 1
+		assert (n_coordcols == 2*self.nref), "number of columns in the pattern doesn't match the number of reference points listed"
+
+		
+		for i, r in enumerate(refs):
+			if (i==0):
+				self.patt['x'] += coords[r]['x']
+				self.patt['y'] += coords[r]['y']
+			else:
+				colx = 'x{0}'.format(i)
+				coly = 'y{0}'.format(i)
+				self.patt[colx] += coords[r]['x']
+				self.patt[coly] += coords[r]['y']
+		
+		# now update the frame attribute from relative to absolute:
+		self.frame = '{0}-abs'.format(self.frame[0][:3])
+			
+		return
+		
+		
+#===============================================================================================					
+	
+	def plot(self, out=None):
+		
+		'''
+		This function will visualize the pattern, in the coordinate system provided. If the 'ref' attribute contains more than 1 entry, then the pattern will be duplicated and translated to all locations.
+		
+		Parameters:
+		-----------
+		- out: 	if a filename is provided, the plot will be saved to this file. Default = None. (string)
+		
+		Future work:
+		------------
+		Specify the coordinate frame and perform the conversion before plotting.
+		
+		
+		'''
+		
+		# generate the key coordinates
+		coords = lrs_gencoords(mode=self.mode, frame=self.frame[0][:3])
+		
+		# figure out if there are multiple refrence positions
+		ref_tmp = (self.ref[0]).split(',')
+		# strip out whitespaces in case there are any:
+		refs = [r.strip() for r in ref_tmp]
+		nref = len(refs)
+		print(refs)
+		pdb.set_trace()
+		
+		
+		
+		fig, ax = plt.subplots(figsize=[12,4])
+		
+		if (self.frame == 'det-abs') or (self.frame == 'det-rel'):
+			units = 'pixels'
+			pltframe = 'det'
+		else:
+			units = 'arcsec'
+			pltframe = self.frame
+		
+		# check whether the pattern is for slit or slitless
+		if (self.mode == 'slit'):
+			coords = lrs_gencoords(mode='slit', frame=pltframe)
+			
+			# if the coordinates are in relative pixel coordinates, translate the pattern 
+			if (self.frame == 'det-rel'):
+				self.to_absolute()
+			
+			cornersx = np.array((coords['ll']['x'], coords['ul']['x'], coords['ur']['x'], coords['lr']['x']))
+			cornersy = np.array((coords['ll']['y'], coords['ul']['y'], coords['ur']['y'], coords['lr']['y']))
+			corners = np.stack((cornersx, cornersy), axis=-1)
+			# create the matplotlib patch for plotting
+			slit_rect = Polygon(corners, closed=True, lw=2., color='g', fill=False, label='slit edge')
+			ax.scatter(cornersx, cornersy, marker='+', color='g')
+			ax.scatter(coords['c']['x'], coords['c']['y'], marker='o', color='r', label='slit centre')
+			for n in ['nod1', 'nod2']:
+				ax.scatter(coords[n]['x'], coords[n]['y'], marker='o', edgecolor='r', facecolor='white')
+			ax.add_patch(slit_rect)
+			
+		elif (self.mode == 'slitless'):
+			coords = lrs_gencoords(mode='slitless', frame=pltframe)
+			
+			# as above, if the points are given in relative pixel values, translate the pattern
+			if (self.frame == 'det-rel'):
+				self.to_absolute()
+		
+		if (len(self.ref) == 1):
+			
+			# if there's only 1 reference point, we don't need to expand the pattern to account for different reference points
+			# if there's only 1 reference point we assume it's the centre/nominal pointing location
+			
+			#indx = np.arange(len(self.patt))
+			pts = ax.scatter(self.patt['x'], self.patt['y'], marker='x', c=self.patt['Pointing'], cmap='plasma', label='pointings ({})'.format(self.npts))
+			cbar = fig.colorbar(pts, ax=ax)
+			cbar.set_label('pointing index')
+			ax.grid(color='k', linestyle='-', linewidth=0.5, alpha=0.5)
+			ax.set_xlabel(units)
+			ax.set_ylabel(units)
+			ax.legend(loc='upper right')
+				 
+		
+		ax.set_title(self.name)
+		
+
+		if (out is not None):
+			fig.savefig(out)
+    
+		fig.show()
+    
+		return fig
+		
+		
 	def run_checks(self):
 		
-		'''In this function we will run some sanity checks on the coordinates and reference frames provided, so that we don't waste time or create unrealistic patterns.
+		'''In this function we will run some sanity checks on the coordinates and reference frames provided, so that we don't waste time or create unrealistic patterns. DOESN'T CURRENTLY WORK.
 		
 		Parameters:
 		-----------
@@ -175,151 +320,8 @@ class LRSPattern(object):
 				assert (np.all(self.patt[c]) >= -436.) and (np.all(self.patt[c]) <= -314.), "Coordinate frame/values inconsistency: Telescope coordinates out of range of the MIRI Imager detector"
 			
 			
-		
-		
-		
-		
-			
-	
-	def to_absolute(self):
-		
-		'''
-		This function will change the coordinates from relative to absolute. This is ONLY applicable for patterns defined in the 'det-rel' frame; it will take the pattern, identify the reference coordinates for the given mode, and calculate the absolute detector pixels.
-		
-		Parameters:
-		-----------
-		None
-		
-		Returns:
-		--------
-		- the input pattern with updated positions, and the reference frame set to 'det-abs' in the metadata
-		
-		
-		'''
-		
-		# first check that the input pattern is defined in relative coordinates
-		assert ('rel' in self.frame[0]), "The input pattern is not in relative coordinates!"
-		
-		print('Converting pattern to absolute coordinates.....')
-		# if the input coordinates are relative, load the coordinates so we can do the translation
-		coords = lrs_gencoords(mode=self.mode, frame=self.frame[0], verbose=False)
-		
-		# convert the reference attribute to a list
-		ref_tmp = (self.ref[0]).split(',')
-		# strip out whitespaces in case there are any:
-		refs = [r.strip() for r in ref_tmp]
-		
-		# the (number of columns - 1) needs to be == (2 * the number of entries in the reference list)
-		n_coordcols = len(self.patt.colnames) - 1
-		assert (n_coordcols == 2*self.nref), "number of columns in the pattern doesn't match the number of reference points listed"
-		
+		return
 
-		for i, r in enumerate(refs):
-			if (i==0):
-				self.patt['x'] += coords[r]['x']
-				self.patt['y'] += coords[r]['y']
-			else:
-				colx = 'x{0}'.format(i)
-				coly = 'y{0}'.format(i)
-				self.patt[colx] += coords[r]['x']
-				self.patt[coly] += coords[r]['y']
-		
-		# now update the frame attribute from relative to absolute:
-		self.frame = '{0}-abs'.format(self.frame[0][:3])
-		
-		
-			
-	
-	def plot(self, out=None):
-		
-		'''
-		This function will visualize the pattern, in the coordinate system provided. If the 'ref' attribute contains more than 1 entry, then the pattern will be duplicated and translated to all locations.
-		
-		Parameters:
-		-----------
-		- out: 	if a filename is provided, the plot will be saved to this file. Default = None. (string)
-		
-		Future work:
-		------------
-		Specify the coordinate frame and perform the conversion before plotting.
-		
-		
-		'''
-		
-		# generate the key coordinates
-		coords = lrs_gencoords(mode=self.mode, frame=self.frame[:3])
-		
-		# figure out if there are multiple refrence positions
-		ref_tmp = (self.ref[0]).split(',')
-		# strip out whitespaces in case there are any:
-		refs = [r.strip() for r in ref_tmp]
-		nref = len(refs)
-		print(refs)
-		pdb.set_trace()
-		
-		
-		
-		fig, ax = plt.subplots(figsize=[12,4])
-		
-		if (self.frame == 'det-abs') or (self.frame == 'det-rel'):
-			units = 'pixels'
-		else:
-			units = 'arcsec'
-		
-		# check whether the pattern is for slit or slitless
-		if (self.mode == 'slit'):
-			coords = lrs_gencoords(mode='slit', frame=pltframe)
-			
-			# if the coordinates are in relative pixel coordinates, translate the pattern 
-			if (self.frame == 'det-rel'):
-				self.patt['x'] += coords['c']['x']
-				self.patt['y'] += coords['c']['y']
-			
-			cornersx = np.array((coords['ll']['x'], coords['ul']['x'], coords['ur']['x'], coords['lr']['x']))
-			cornersy = np.array((coords['ll']['y'], coords['ul']['y'], coords['ur']['y'], coords['lr']['y']))
-			corners = np.stack((cornersx, cornersy), axis=-1)
-			# create the matplotlib patch for plotting
-			slit_rect = Polygon(corners, closed=True, lw=2., color='g', fill=False, label='slit edge')
-			ax.scatter(cornersx, cornersy, marker='+', color='g')
-			ax.scatter(coords['c']['x'], coords['c']['y'], marker='o', color='r', label='slit centre')
-			for n in ['nod1', 'nod2']:
-				ax.scatter(coords[n]['x'], coords[n]['y'], marker='o', edgecolor='r', facecolor='white')
-			ax.add_patch(slit_rect)
-			
-		elif (self.mode == 'slitless'):
-			coords = lrs_gencoords(mode='slitless', frame=pltframe)
-			
-			# as above, if the points are given in relative pixel values, translate the pattern
-			if (self.frame == 'det-rel'):
-				self.patt['x'] += coords['c']['x']
-				self.patt['y'] += coords['c']['y']
-		
-		if (len(self.ref) == 1):
-			
-			# if there's only 1 reference point, we don't need to expand the pattern to account for different reference points
-			# if there's only 1 reference point we assume it's the centre/nominal pointing location
-			
-			#indx = np.arange(len(self.patt))
-			pts = ax.scatter(self.patt['x'], self.patt['y'], marker='x', c=self.patt['Pointing'], cmap='plasma', label='pointings ({})'.format(self.npts))
-			cbar = fig.colorbar(pts, ax=ax)
-			cbar.set_label('pointing index')
-			ax.grid(color='k', linestyle='-', linewidth=0.5, alpha=0.5)
-			ax.set_xlabel(units)
-			ax.set_ylabel(units)
-			ax.legend(loc='upper right')
-				 
-		
-		ax.set_title(self.name)
-		
-
-		if (out is not None):
-			fig.savefig(out)
-    
-		fig.show()
-    
-		return fig
-		
-		
 
 
 #----------------------------------------------------------------------------	
@@ -331,7 +333,7 @@ def lrs_gencoords(mode='slit', frame='tel', plot=False, verbose=False):
 	Parameters:
 	-----------
 	- mode: 'slit' (default) or 'slitless'. this will determine which set of coordinates is returned
-	- frame: the reference frame in which the coordinates are specified. Options: 'det-abs' (detector [pixels], absolute), 'det-rel' (detector [pixels], relative) 'idl' (Ideal [arcsec]), 'tel' (v2v3 [arcsec])
+	- frame: the reference frame in which the coordinates are specified. Options: 'det' (detector [pixels]), 'idl' (Ideal [arcsec]), 'tel' (v2v3 [arcsec])
 	- verbose: prints some output to screen (boolean, default False)
 	
 	
@@ -343,7 +345,7 @@ def lrs_gencoords(mode='slit', frame='tel', plot=False, verbose=False):
 	
 	# checks that the parameters provided are valid
 	assert (mode in ['slit', 'slitless']), "Mode not recognised. Options are: 'slit', 'slitless'. "
-	assert (frame in ['tel', 'idl', 'det-abs', 'det-rel']), "Coordinate frame not recognised. Options are: 'det-rel' (detector relative), 'det-abs' (detector absolute), 'idl' (ideal), 'tel' (telescope/v2v3)."
+	assert (frame in ['tel', 'idl', 'det']), "Coordinate frame not recognised. Options are: 'det' (detector), 'idl' (ideal), 'tel' (telescope/v2v3)."
 	
 	# the read_aperture function (below) converts 'slit' or 'slitless' to the appropriate SIAF aperture name.
 	ap = read_aperture(mode=mode)
